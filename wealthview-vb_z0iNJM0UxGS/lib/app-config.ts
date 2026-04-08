cd1d92793d97ba58509e440c2bf5150ef283d226
Henro1975 <94793678+Henro1975@users.noreply.github.com>
2026-04-08 15:09:59 +0100

    Update app-config.ts

--- wealthview-vb_z0iNJM0UxGS/lib/app-config.ts
+++ wealthview-vb_z0iNJM0UxGS/lib/app-config.ts
@@ -4,12 +4,12 @@
 export const APP_CONFIG = {
   // UPDATE: Set to the welcome message for the chatbot
   WELCOME_MESSAGE:
-    'Welcome!
+    `Welcome!
 I'm your 24/7 financial guide, here to help you build wealth & achieve financial freedom. Let's explore financial education & investment insights to reach your goals.
-How can I help u today?',
+How can I help u today?`,
 
   // UPDATE: Set to the name of the chatbot app
-  NAME: "WealthView ',
+  NAME: "WealthView `,
 
   // UPDATE: Set to the description of the chatbot app
   DESCRIPTION: "Discover elite financial education & expert investment guidance—your key to building wealth and achieving true financial freedom. 24/7 personalized support for all your goals. Join now!",
@@ -18,8 +18,8 @@ How can I help u today?',
 // Colors Configuration - UPDATE THESE VALUES BASED ON USER DESIGN PREFERENCES
 export const COLORS = {
   // UPDATE: Set to the background color (hex format)
-  BACKGROUND: "#FFFFFF',
+  BACKGROUND: "#FFFFFF`,
 
   // UPDATE: Set to the primary color for buttons, links, etc. (hex format)
-  PRIMARY: "#703D92',
+  PRIMARY: "#703D92`,
 } as const;
