db5e9a6560342f58e502a8956cde6fa31704274b
Henro1975 <94793678+Henro1975@users.noreply.github.com>
2026-05-07 11:28:11 +0100

    Update layout.tsx

--- wealthview-vb_z0iNJM0UxGS/app/layout.tsx
+++ wealthview-vb_z0iNJM0UxGS/app/layout.tsx
@@ -1,3 +1,27 @@
+"use client"; // If it's already there
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Script from "next/script"; // <--- THIS MUST BE AT THE TOP
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <Script 
+          src="https://sdk.minepi.com/pi-sdk.js" 
+          strategy="beforeInteractive" 
+        />
+      </head>
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
 import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
